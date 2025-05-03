470efc378ca28fcdc3a804c8f235b038a7c6dbbe
云舒 <1315645341@qq.com>
2025-05-03 23:52:59 +0800

    test

---
+++ Runtime/Script/Component/UI/ToggleExpand.cs
@@ -0,0 +1,44 @@
+#region Copyright
+
+// **********************************************************************
+// Copyright (C) 2023 HeJing
+//
+// Script Name :		ToggleExpand.cs
+// Author Name :		YunShu
+// Create Time :		2023/09/22 10:34:27
+// Description :
+// **********************************************************************
+
+#endregion
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameWorldFramework.RunTime.Component.UI
+{
+    public static class ToggleExpand
+    {
+        /// <summary>
+        /// 设置 toggle 状态 并触发 on Value Change
+        /// </summary>
+        /// <param name="_toggle"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsOn(this Toggle _toggle, bool value)
+        {
+            if (_toggle.isOn == value)
+            {
+                _toggle.onValueChanged.Invoke(value);
+            }
+            else
+            {
+                _toggle.isOn = value;
+            }
+            
+            return value;
+        }
+    }
+}
---
+++ Runtime/Script/Utility/AssetBundle/AssetBundleNameDrawer.cs
@@ -0,0 +1,94 @@
+﻿#region Copyright
+// **********************************************************************
+// Copyright (C) 2022 HeJing
+//
+// Script Name :		AssetBundleNameDraw.cs
+// Author Name :		YunShu
+// Create Time :		2022/12/06 10:45:03
+// Description :
+// **********************************************************************
+#endregion
+
+
+
+using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace GameWorldFramework.RunTime.Utility
+{
+    [CustomPropertyDrawer(typeof(AssetBundleNameAttribute))]
+    public class AssetBundleNameDrawer : PropertyDrawer
+    {
+        public string[] Names
+        {
+            get
+            {
+                
+                return AssetDatabase.GetAllAssetBundleNames();
+            }
+        }
+
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            
+            string[] names = Names;
+
+            ///初始值为空bug
+            if (property.stringValue == "")
+            {
+
+                if (names.Length > 0)
+                {
+                    property.stringValue = names[0];
+
+                }
+                else
+                {
+                    EditorGUI.Popup(position, property.displayName, 0, new string[] { "" });
+                }
+
+                return;
+            }
+
+
+            if (names.Length > 0 && property.stringValue != null)
+            {
+                int num = Array.IndexOf(names, property.stringValue);
+
+                if (num == -1)
+                {
+                    if (names.Length > 0)
+                    {
+                        property.stringValue = names[0];
+
+                    }
+                    else
+                    {
+                        EditorGUI.Popup(position, property.displayName, 0, new string[] { "" });
+                    }
+                }
+                else
+                {
+                    num = EditorGUI.Popup(position, property.displayName, num, names);
+                    property.stringValue = names[num];
+                }
+
+                return;
+            }
+
+            if (names.Length == 0)
+            {
+                EditorGUI.Popup(position, property.displayName, 0, new string[] { "" });
+                return;
+            }
+
+            base.OnGUI(position, property, label);
+        }
+    }
+
+}
+# endif
